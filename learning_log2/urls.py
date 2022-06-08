5e32b8e8adbd38b982c4e43abac805de3df43e17
MisterMoon1 <zebuht88@mail.ru>
2022-06-08 11:46:09 +0500

    Added topics

--- learning_log2/urls.py
+++ learning_log2/urls.py
@@ -6,6 +6,10 @@ from . import views
 
 app_name = 'learning_log2'
 urlpatterns = [
-    #Домашняя страница
-    path('', views.index, name='index')
+    # Домашняя страница
+    path('', views.index, name='index'),
+    # Страница со списком всех тем
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной информацией по отдельной теме
+    path('topics/<int:topic_id>/', views.topics, name='topics')
 ]
